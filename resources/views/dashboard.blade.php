e5b082405462c8957f41c741a38654203ca88ca7
fidelwx <fidel_herrera128@hotmail.com>
2019-02-16 14:47:16 -0400

    arreglo de graficas

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -181,68 +181,6 @@
       </div>
       <!--Grid row-->
   @section('my-js')
-<<<<<<< HEAD
-    <!-- Charts -->
-    <script>
-      // Line
-      var ctx = document.getElementById("myChart").getContext('2d');
-      var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-          datasets: [{
-            label: 'Estadisticas generales',
-            data: [12, 19, 3, 5, 2, 30],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-              'rgba(255,99,132,1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: true
-              }
-            }]
-          }
-        }
-      });
-
-      //pie
-      var ctxP = document.getElementById("pieChart").getContext('2d');
-      var myPieChart = new Chart(ctxP, {
-          type: 'pie',
-          data: {
-            labels: ["Productos", "Entradas", "Salidas"],
-            datasets: [{
-              data: [300, 50, 100],
-              backgroundColor: ["#ff3547", "#00c851", "#4285f4"],
-              hoverBackgroundColor: ["#ff3560", "#009751", "#4265f4"]
-            }]
-          },
-          options: {
-            responsive: true,
-            legend: false
-          }
-        });
-    </script>
-    <!-- Charts -->
-=======
   	<!-- Charts -->
 	  <script>
 	    // Line
@@ -339,6 +277,5 @@
 
 	  </script>
   	<!-- Charts -->
->>>>>>> 7dcf367c542478400d175ddf7fc8309d4aa5aae4
   @stop
 @stop
