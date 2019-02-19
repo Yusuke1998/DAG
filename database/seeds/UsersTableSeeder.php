3d101d805fd0a483a61c0e2e6bf7c0b2956de65b
Yusuke1998 <jhonnyjose1998@gmail.com>
2019-02-19 18:14:15 -0400

    user=admin, pass=admin

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -7,7 +7,7 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         $usuario = User::create([
-        	'name'		=>	'administrador',
+        	'name'		=>	'admin',
         	'email'		=>	'admin@admin.com',
         	'password'	=>	'$2y$10$axUjxmv8ugPjwtLPaE0DGOqLndcNWDihBqOlFnonugEQp3DTvDG1u',
         	'type'		=>	'administrador',
